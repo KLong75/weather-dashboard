4d880c55d3608f9764131c52ded94879d7b9d595
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-03-17 13:39:57 -0500

    set search button to capture user input and use the user input to make API call to get weather information for the chosen city

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,31 @@
-var apiUrl ="https://api.openweathermap.org/data/2.5/weather?q=Austin&appid=523a8452a92059fd3b4bc789dcceccb3"
+var citySearchFormEl = document.querySelector("#city-search-form");
+var cityInputEl = document.querySelector("#city-search");
+var currentWeatherEl = document.querySelector("#current-weather");
+var fiveDayForecastEl = document.querySelector("#future-forecast")
+var searchHistoryEl = document.querySelector("#search-history-box")
+
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  var city = cityInputEl.value.trim();
+    if (city) {
+      searchCityWeather(city);
+      currentWeatherEl.textContent = "";
+      fiveDayForecastEl.textContent = "";
+      cityInputEl.value = "";
+    } else {
+      alert ("Please enter the name of a city.")
+    }
+};
+
+
+
+
+
+var searchCityWeather = function(city) {
+  console.log(city);
+
+var apiUrl ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=523a8452a92059fd3b4bc789dcceccb3";
 fetch(apiUrl)
       .then(function(response) {
         // request was succesful
@@ -14,3 +41,7 @@ fetch(apiUrl)
         // Notice this `.catch()` getting chained onto the end of the `.then()` method
         alert("Unable to connect");
       });
+
+}
+
+citySearchFormEl.addEventListener("submit", formSubmitHandler);
